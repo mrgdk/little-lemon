19e0ad28b64591b4d7b5ec66cb05fa13bfff0595
mrgdk <e.f.guduk@gmail.com>
2024-11-29 21:24:07 +0100

    user view

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,13 @@
 #define URL route for index() view
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('', include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
 from .models import Menu, Booking
-from .serializers import MenuSerializer, BookingSerializer
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 
-# Create your views here. 
+# Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
@@ -15,3 +16,8 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()  # Fetch all User objects
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
